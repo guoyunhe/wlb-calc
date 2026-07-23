e584a5225935cf5300a6b2d7472140b05d69c91f
Guo Yunhe <i@guoyunhe.me>
2026-07-23 13:13:26 +0800

    chore: 移除MantineProvider的默认颜色方案和空主题配置
    简化了MantineProvider的初始化参数，移除无意义的空主题和默认自动配色方案设置

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MantineProvider defaultColorScheme="auto" theme={{}}>
+    <MantineProvider>
       <App />
     </MantineProvider>
   </StrictMode>,
