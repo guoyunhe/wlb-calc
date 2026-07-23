87ba6ec4ab33f158dc64ee254f4f3d2d6a690ee4
Guo Yunhe <i@guoyunhe.me>
2026-07-23 13:14:17 +0800

    feat(main.tsx): 给MantineProvider添加自动配色主题配置
    为MantineProvider新增defaultColorScheme="auto"属性，让应用可以自动跟随系统配色方案切换主题

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MantineProvider>
+    <MantineProvider defaultColorScheme="auto">
       <App />
     </MantineProvider>
   </StrictMode>,
